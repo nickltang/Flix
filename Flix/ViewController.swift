732292a72c3b27bcb3184884016754bb3fa812e3
Nick <nicholasltang21@gmail.com>
2020-09-03 16:14:52 -0700

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Nick Tang on 9/3/20.
+//  Copyright © 2020 nick2001tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
